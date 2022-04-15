42945bbaf6a5a3ea84a7ace6293ecaa69b336982
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-04-15 10:04:11 -0600

    commit tree e72b270fe9e8cc357f42371bb32fc9e36c6ec2cc

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData (nameInput, emailInput) {
+    return fetch ('http://localhost:3000/users', {
+        method : 'POST',        
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept' : 'application/json',
+        },
+        body: JSON.stringify({
+            name : nameInput,
+            email : emailInput,
+        })
+    })
+    .then(response => response.json())
+    .then(data => appendData(data.id))
+    .catch((error) => appendData(error.message));
+}
+
+function appendData(data) {
+    const body = document.querySelector('body');
+    body.append(data);
+}
